8aeb3787ce6501ad68ea0a23e7bfdf81afd6979e
Adriancys Villegas <adriancysvillegast@gmail.com>
2024-02-04 14:52:10 -0400

    Initial Commit

---
+++ Movie Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Store
+//
+//  Created by Adriancys Jesus Villegas Toro on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movie StoreUITests/Movie_StoreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movie_StoreUITestsLaunchTests.swift
+//  Movie StoreUITests
+//
+//  Created by Adriancys Jesus Villegas Toro on 4/2/24.
+//
+
+import XCTest
+
+final class Movie_StoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
